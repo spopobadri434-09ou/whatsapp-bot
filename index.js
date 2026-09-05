0c9308a95a2c065ddec5b92c0b9d89055fa96c4b
spopobadri434-09ou <spopobadri434@gmail.com>
2026-09-05 04:35:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ async function startBot() {
 
     if (qr) {
       console.log("امسح رمز QR من واتساب:");
-      qrcode.generate(qr, { small: true });
+      qrcode.generate(qr, { small: false });
     }
 
     if (connection === "open") {
